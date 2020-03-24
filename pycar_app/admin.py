a684a577d426ddabd88f1a4cd28f62fadb9fa288
Vorotynsky Maxim <vorotynsky@niuitmo.ru>
2020-03-24 18:14:33 +0300

    feat: model

--- pycar_app/admin.py
+++ pycar_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Car)
+admin.site.register(Owner)
+admin.site.register(License)
+admin.site.register(OwnerShip)
--- pycar_app/models.py
+++ pycar_app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Car(models.Model):
+    id               = models.CharField(max_length=9, primary_key=True)
+    model            = models.CharField(max_length=32)
+    provider         = models.CharField(max_length=32)
+
+
+class Owner(models.Model):
+    first_name      = models.CharField(max_length=32)
+    last_name       = models.CharField(max_length=48)
+    birthday        = models.DateField()
+    cars            = models.ManyToManyField(Car, through='Ownership')
+
+
+class License(models.Model):
+    TYPES = [('A', 'A'), ('B', 'B'), ('C', 'C'), ('D', 'D')]
+    id              = models.AutoField(primary_key=True)
+    type            = models.CharField(max_length=1, choices=TYPES)
+    owner           = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    issue_date      = models.DateField()
+    expiration_date = models.DateField(null=True)
+
+
+class OwnerShip(models.Model):
+    owner           = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    car             = models.ForeignKey(Car, on_delete=models.CASCADE)
+    start           = models.DateField()
+    finish          = models.DateField()
