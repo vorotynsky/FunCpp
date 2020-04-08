cc8dd4c6a5963e5b81af52925e40b9bdaaf10b77
Vorotynsky Maxim <vorotynsky@niuitmo.ru>
2020-04-08 23:56:21 +0300

    feat: list views for cars and owners

--- pycar_app/urls.py
+++ pycar_app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('car/<str:car_id>/',     views.car_details),
-    path('owner/<int:owner_id>/', views.owner_details)
+    path('car/list',              views.CarList.as_view()),
+    path('owner/<int:owner_id>/', views.owner_details),
+    path('owner/list',            views.owner_list),
 ]
--- pycar_app/views.py
+++ pycar_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import Http404
+from django.views.generic.list import ListView
 
 from .models import *
 
@@ -15,9 +16,20 @@ def owner_details(request, owner_id):
     return render(request, 'owner/detail.html', {'owner': owner})
 
 
+def owner_list(request):
+    return render(request, 'owner/list.html', {'owners': Owner.objects.all()})
+
+
+
 def car_details(request, car_id):
     try:
         car = Car.objects.get(pk=car_id)
     except Car.DoesNotExist:
         raise Http404("Car doesn't exists")
     return render(request, 'car/detail.html', {'car': car})
+
+
+class CarList(ListView):
+    model = Car
+    template_name = 'car/list.html'
+
